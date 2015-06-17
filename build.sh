af92ea009e9aab324a16ce1b5433a95bd65a39c1
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-06-17 20:39:32 +0200

    Déplacement des fichiers sources vers master

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-pandoc resume.md -T "Sébastien Castiel – Développeur web full-stack" -c style.css -s -o index.html
+destdir=$1
+
+if [ -z "$destdir" ]; then
+	echo "Usage: $0 <destination_directory>"
+	exit 1
+fi
+
+if [ ! -d "$destdir" ]; then
+	echo "$destdir is not a directory."
+	exit 1
+fi
+
+cp style.css "$destdir"
+mkdir -p "$destdir/fr"
+pandoc resume-fr.md -T "Sébastien Castiel – Développeur web full-stack" -c "../style.css" -s -o "$destdir/fr/index.html"
+mkdir -p "$destdir/en"
+pandoc resume-en.md -T "Sebastien Castiel – Full-stack web developer" -c "../style.css" -s -o "$destdir/en/index.html"
